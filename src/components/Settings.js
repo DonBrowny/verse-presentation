38e63ba35359a39375bc5f7cb45d6b2b8488eec3
DonBrowny <kishore13ask@gmail.com>
2021-05-10 14:41:06 +0530

    added sample settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Settings.scss';
 
 const Settings = () => {
   return (
-    <div>
-      <h2>Settings</h2>
+    <div className="settings">
+      <h3 className="settings-header">Verse Settings</h3>
+      <div className="settings-content">
+        <span>Default Language</span>
+        <select>
+          <option>English</option>
+          <option>தமிழ்</option>
+        </select>
+      </div>
+      <h3 className="settings-header">Lyrics Settings</h3>
+      <div className="settings-content">
+        <span>Default Language</span>
+        <select>
+          <option>English</option>
+          <option>தமிழ்</option>
+        </select>
+      </div>
     </div>
   );
 };
