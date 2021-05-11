83f766e2c235179498e8cb6a3b70d7675eeeadb9
Kishore <kishore13ask@gmail.com>
2021-05-11 17:30:24 +0530

    added useEffect for better render update

--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './Verse.scss';
 import List from './List';
 import { fetchData } from '../utils/utils';
 
-const CHAPTERS = [
+const BOOKS = [
   'GENESIS',
   'EXODUS',
   'LEVITICUS',
@@ -73,30 +73,17 @@ const CHAPTERS = [
   'REVELATION',
 ];
 
-const DEFAULT_VALUE = { data: {}, book: "", chapter: "" };
-
 const Verse = () => {
   const refs = useRef([]);
   const { t, i18n } = useTranslation('verse');
-  const [state, setState] = useState(DEFAULT_VALUE);
 
-  const fetchBook = (path) => {
-    fetchData(`${i18n.language}/${path}`).then((data) => {
-      setState({ data, book: path, chapter: "" });
-    });
-  };
+  const [data, setData] = useState({});
+  const [book, setBook] = useState('');
+  const [chapter, setChapter] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+  const transBook = BOOKS.map((chap) => 'CHAPTERS.' + chap);
 
-  const resetSearch = () => {
-    setState(DEFAULT_VALUE)
-  };
-
-  React.useEffect(() => {
-    return i18n.on('languageChanged', (lng) => {
-      resetSearch();
-    });
-  }, []);
-
-  const result = (resultList) => {
+  const arrayToDiv = (resultList) => {
     if (resultList.length && resultList.length > 0) {
       return resultList.map((verse, index) => {
         return (
@@ -106,7 +93,7 @@ const Verse = () => {
             ref={(element) => {
               refs.current[index] = element;
             }}
-            className="card "
+            className="card"
           >
             <b>{index + 1}.</b> {verse.Verse}
           </div>
@@ -115,14 +102,30 @@ const Verse = () => {
     }
   };
 
+  useEffect(() => {
+    let cancel = false;
+    if (!book) return;
+    fetchData(`${i18n.language}/${book}`).then((data) => {
+      if (cancel) return
+      setData(data);
+    });
+    return () => cancel = true;
+  }, [book, i18n.language])
+
+  useEffect(() => {
+    let result = data.Chapter && chapter ? data.Chapter[chapter].Verse : '';
+    setSearchResult(result)
+  }, [data, chapter])
+
   const onFormChange = (key, value) => {
     switch (key) {
       case 'book':
-        fetchBook(value);
+        setBook(value);
+        setChapter('');
         break;
 
       case 'chapter':
-        setState({ ...state, chapter: value });
+        setChapter(value);
         break;
 
       case 'verse':
@@ -141,10 +144,6 @@ const Verse = () => {
     console.log(verse);
   };
 
-  const transChapters = () => {
-    return CHAPTERS.map((chap) => 'CHAPTERS.' + chap);
-  };
-
   return (
     <article className="verse flex-stretch">
       <section className="verse-search">
@@ -154,30 +153,30 @@ const Verse = () => {
         <span>{t('FORM.VERSE')}:</span>
         <List
           listId="book"
-          selected={state.book}
-          items={transChapters()}
+          selected={book}
+          items={transBook}
           translation="true"
           onSelectionChange={onFormChange}
         />
         <List
           listId="chapter"
-          selected={state.chapter}
-          items={state.data.Chapter && state.data.Chapter.length}
+          selected={chapter}
+          items={data.Chapter && data.Chapter.length}
           onSelectionChange={onFormChange}
         />
         <List
           listId="verse"
-          items={state.chapter && state.data.Chapter[state.chapter].Verse.length}
+          items={chapter && data.Chapter[chapter].Verse.length}
           onSelectionChange={onFormChange}
         />
       </section>
       <section className="verse-results">
         <h3>{t('FORM.SEARCH_RESULT')}</h3>
         <h3>
-          {state.chapter && `${t('CHAPTERS.' + CHAPTERS[state.book])}  ${state.chapter + 1}`}
+          {chapter && `${t('CHAPTERS.' + BOOKS[book])}  ${chapter + 1}`}
         </h3>
         <div className="results-list" onClick={searchSelect}>
-          {state.chapter && result(state.data.Chapter[state.chapter].Verse)}
+          {searchResult && arrayToDiv(searchResult)}
         </div>
       </section>
     </article>
