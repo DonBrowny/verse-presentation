a7c8ef7730ab035e6e2e7bdaa51d2cfe2e2e7e05
DonBrowny <kishore13ask@gmail.com>
2021-05-14 09:52:40 +0530

    word search wireframe

--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,18 +1,28 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import './Verse.scss';
-import List from './List';
-import { fetchData } from '../utils/utils';
 import { BOOKS } from '../utils/constants';
+import VerseSearch from './VerseSearch';
 
 const Verse = () => {
   const refs = useRef([]);
-  const { t, i18n } = useTranslation('verse');
-
-  const [data, setData] = useState({});
-  const [book, setBook] = useState('');
-  const [chapter, setChapter] = useState('');
+  const { t } = useTranslation('verse');
   const [searchResult, setSearchResult] = useState('');
+
+  const stringifyVerse = function (resultList) {
+    if (resultList.length && resultList.length > 0) {
+      const id = resultList[0].Verseid;
+      const book = parseInt(id.substring(0, 2), 10);
+      const chapter = parseInt(id.substring(0, 2), 10);
+      return `${t(BOOKS[book])}  ${chapter + 1}`;
+    }
+  };
+
+  const onVerseClick = useCallback((value) => {
+    setTimeout(() => {
+      refs.current[value].scrollIntoView({ behavior: 'smooth' });
+    });
+  }, []);
 
   const arrayToDiv = (resultList) => {
     if (resultList.length && resultList.length > 0) {
@@ -33,43 +43,6 @@ const Verse = () => {
     }
   };
 
-  useEffect(() => {
-    let cancel = false;
-    if (!book) return;
-    fetchData(`${i18n.language}/${book}`).then((data) => {
-      if (cancel) return;
-      setData(data);
-    });
-    return () => (cancel = true);
-  }, [book, i18n.language]);
-
-  useEffect(() => {
-    let result = data.Chapter && chapter ? data.Chapter[chapter].Verse : '';
-    setSearchResult(result);
-  }, [data, chapter]);
-
-  const onFormChange = useCallback((key, value) => {
-    switch (key) {
-      case 'book':
-        setBook(value);
-        setChapter('');
-        break;
-
-      case 'chapter':
-        setChapter(value);
-        break;
-
-      case 'verse':
-        setTimeout(() => {
-          refs.current[value].scrollIntoView({ behavior: 'smooth' });
-        });
-        break;
-
-      default:
-        break;
-    }
-  }, []);
-
   const searchSelect = (event) => {
     let verse = event.target.getAttribute('data-verse');
     console.log(verse);
@@ -77,33 +50,13 @@ const Verse = () => {
 
   return (
     <article className="verse flex-stretch">
-      <section className="verse-search">
-        <h3>{t('FORM.VERSE_SEARCH')}</h3>
-        <span>{t('FORM.BOOK')}:</span>
-        <span>{t('FORM.CHAPTER')}:</span>
-        <span>{t('FORM.VERSE')}:</span>
-        <List
-          listId="book"
-          selected={book}
-          items={BOOKS}
-          translation="true"
-          onSelectionChange={onFormChange}
-        />
-        <List
-          listId="chapter"
-          selected={chapter}
-          items={data.Chapter && data.Chapter.length}
-          onSelectionChange={onFormChange}
-        />
-        <List
-          listId="verse"
-          items={chapter && data.Chapter[chapter].Verse.length}
-          onSelectionChange={onFormChange}
-        />
-      </section>
+      <VerseSearch
+        onResultAvailable={setSearchResult}
+        onVerseClick={onVerseClick}
+      />
       <section className="verse-results">
         <h3>{t('FORM.SEARCH_RESULT')}</h3>
-        <h3>{chapter && `${t(BOOKS[book])}  ${chapter + 1}`}</h3>
+        <h3>{searchResult && stringifyVerse(searchResult)}</h3>
         <div className="results-list" onClick={searchSelect}>
           {searchResult && arrayToDiv(searchResult)}
         </div>
---
+++ src/components/VerseSearch.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import List from './List';
+import { useTranslation } from 'react-i18next';
+import { fetchData } from '../utils/utils';
+import { BOOKS } from '../utils/constants';
+import './VerseSearch.scss';
+
+function VerseSearch({ onResultAvailable, onVerseClick }) {
+  const { t, i18n } = useTranslation('verse');
+  const [data, setData] = useState({});
+  const [book, setBook] = useState('');
+  const [chapter, setChapter] = useState('');
+
+  useEffect(() => {
+    let result = data.Chapter && chapter ? data.Chapter[chapter].Verse : '';
+    onResultAvailable(result);
+  }, [data, chapter, onResultAvailable]);
+
+  useEffect(() => {
+    let cancel = false;
+    if (!book) return;
+    fetchData(`${i18n.language}/${book}`).then((data) => {
+      if (cancel) return;
+      setData(data);
+    });
+    return () => (cancel = true);
+  }, [book, i18n.language]);
+
+  const onFormChange = useCallback(
+    (key, value) => {
+      switch (key) {
+        case 'book':
+          setBook(value);
+          setChapter('');
+          break;
+
+        case 'chapter':
+          setChapter(value);
+          break;
+
+        case 'verse':
+          onVerseClick(value);
+          break;
+
+        default:
+          break;
+      }
+    },
+    [onVerseClick]
+  );
+  return (
+    <section className="verse-search">
+      <h3 className="row">{t('FORM.VERSE_SEARCH')}</h3>
+      <span>{t('FORM.BOOK')}:</span>
+      <span>{t('FORM.CHAPTER')}:</span>
+      <span>{t('FORM.VERSE')}:</span>
+      <List
+        listId="book"
+        selected={book}
+        items={BOOKS}
+        translation="true"
+        onSelectionChange={onFormChange}
+      />
+      <List
+        listId="chapter"
+        selected={chapter}
+        items={data.Chapter && data.Chapter.length}
+        onSelectionChange={onFormChange}
+      />
+      <List
+        listId="verse"
+        items={chapter && data.Chapter[chapter].Verse.length}
+        onSelectionChange={onFormChange}
+      />
+      <span className="row bold">{t('FORM.WORD_SEARCH_IN')}:</span>
+      <div className="row">{t('FORM.WORD_SEARCH')}:</div>
+      <span className="row bold">{t('FORM.WORD_SEARCH')}:</span>
+      <input className="row-2" type="text" name="" id="" />
+      <button className="icon-button">
+        <img src="/icons/search-solid.svg" alt="Search" />
+      </button>
+    </section>
+  );
+}
+
+export default VerseSearch;
