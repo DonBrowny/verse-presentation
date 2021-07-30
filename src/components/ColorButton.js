df40ffff29eea0dfdd9db68a618d24f243e79f45
DonBrowny <kishore13ask@gmail.com>
2021-07-30 19:35:18 +0530

    Added colour buttons

---
+++ src/components/ColorButton.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
+import './ColorButton.scss';
+
+const ColorButton = ({ selectedColor = undefined, onChangeCompleted }) => {
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const [color, setColor] = useState(
+    selectedColor || { r: '241', g: '112', b: '19', a: '1' }
+  );
+
+  const openPicker = () => {
+    setDisplayColorPicker(true);
+  };
+
+  const closePicker = () => {
+    setDisplayColorPicker(false);
+    onChangeCompleted(color);
+  };
+
+  const handleChange = (color) => {
+    setColor(color.rgb);
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
+        }}
+        className="swatch"
+        onClick={openPicker}
+      >
+        <div className="color" />
+      </div>
+      {displayColorPicker && (
+        <div className="popover">
+          <div className="cover" onClick={closePicker} />
+          <SketchPicker color={color} onChange={handleChange} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ColorButton;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ColorButton from './ColorButton';
 import './Settings.scss';
 
 const Settings = () => {
@@ -19,6 +20,40 @@ const Settings = () => {
           <option>English</option>
           <option>தமிழ்</option>
         </select>
+      </div>
+
+      <h3 className="settings-header">Presentation Settings</h3>
+      <div className="settings-content">
+        <span>Minimum Font Size</span>
+        <input type="number" defaultValue="40" step="1" />
+      </div>
+      <div className="settings-content">
+        <span>Maximum Font Size</span>
+        <input type="number" defaultValue="60" step="1" />
+      </div>
+      <div className="settings-content">
+        <span>Background color</span>
+        <ColorButton
+          onChangeCompleted={(color) => {
+            console.log(color);
+          }}
+        />
+      </div>
+      <div className="settings-content">
+        <span>Header color</span>
+        <ColorButton
+          onChangeCompleted={(color) => {
+            console.log(color);
+          }}
+        />
+      </div>
+      <div className="settings-content">
+        <span>Content color</span>
+        <ColorButton
+          onChangeCompleted={(color) => {
+            console.log(color);
+          }}
+        />
       </div>
     </div>
   );
