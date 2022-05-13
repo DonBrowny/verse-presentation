9832703ac36105837be47f07a3ac70cb693e0f73
DonBrowny <kishore13ask@gmail.com>
2022-05-13 08:41:48 +0530

    lint and prettier fixes

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,41 +1,57 @@
-import React, { useRef, useState, forwardRef, useImperativeHandle } from 'react'
+import React, {
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
 import { Input } from 'antd';
 import './Dropdown.scss';
 
-const Option = forwardRef(({text, value, selected}, ref) => {
-    const [focus, setFocus] = useState(false);
-    useImperativeHandle(ref, () => ({
-        setFocus() {
-            setFocus(true);
-        }
-      }));
-    return <li className={`option ${focus ? 'focus' : ''}`} key={value} value={value}>{text}</li>
-})
+const Option = forwardRef(({ text, value, selected }, ref) => {
+  const [focus, setFocus] = useState(false);
+  useImperativeHandle(ref, () => ({
+    setFocus() {
+      setFocus(true);
+    },
+  }));
+  return (
+    <li className={`option ${focus ? 'focus' : ''}`} value={value}>
+      {text}
+    </li>
+  );
+});
+Option.displayName = 'Option';
 
-const Dropdown = ({placeholder, options}) => {
-    const inputRef = useRef(null);
-    const itemEls = useRef([])
-    inputRef.current?.focus({
-        cursor: 'all',
-      });
-      
-    function createOption(options){
-        return options.map(option => <Option {...option} ref={(element) => itemEls.current.push(element)}></Option>)
-    }
+const Dropdown = ({ placeholder, options }) => {
+  const inputRef = useRef(null);
+  const itemEls = useRef([]);
+  inputRef.current?.focus({
+    cursor: 'all',
+  });
 
-    function focusOnFirstOption(e){
-        itemEls.current[0].setFocus();
-        e.target.blur();
-    }
+  function createOption(options) {
+    return options.map((option) => (
+      <Option
+        {...option}
+        key={option.value}
+        ref={(element) => itemEls.current.push(element)}
+      ></Option>
+    ));
+  }
 
-    return (
-        <div className='dropdown'>
-            <Input ref={inputRef} placeholder={placeholder}/>
-            <ul tabIndex={0} onFocus={focusOnFirstOption}>
-                {createOption(options)}
-            </ul>
-        </div>
-    )
-}
+  function focusOnFirstOption(e) {
+    itemEls.current[0].setFocus();
+    e.target.blur();
+  }
 
-export default Dropdown
+  return (
+    <div className='dropdown'>
+      <Input ref={inputRef} placeholder={placeholder} />
+      <ul tabIndex={0} onFocus={focusOnFirstOption}>
+        {createOption(options)}
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Dropdown from './Dropdown';
 
 const Lyrics = () => {
-  const data = [{text: 'Test1', value: 'Test1'},{text: 'Test2', value: 'Test2'},{text: 'Test3', value: 'Test3'}]
+  const data = [
+    { text: 'Test1', value: 'Test1' },
+    { text: 'Test2', value: 'Test2' },
+    { text: 'Test3', value: 'Test3' },
+  ];
   return (
     <div>
       <h2>Lyrics</h2>
-      <Dropdown placeholder="Book" options={data}/>
+      <Dropdown placeholder='Book' options={data} />
     </div>
   );
 };
